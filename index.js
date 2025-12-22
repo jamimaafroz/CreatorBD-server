a79bfaef65717b84c87de526d02d8b73598cd051
jamimaafroz <jamimaafroz123@gmail.com>
2025-12-23 01:13:02 +0600

    success

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.on3ghb8.mongodb.net/?appName=Cluster0`;
 
-
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
-
+//DB_USER=creatorBd_User
+// DB_PASSWORD=A4xEC1iYVmVucem5
 async function run() {
-    try {
+  try {
+    await client.connect();
+    const creatorCollection = client.db("CreatorDB").collection("users");
 
-        await client.connect();
-        const creatorCollection = client.db("CreatorDB").collection("users");
-
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-
-    }
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
 }
 run().catch(console.dir);
 
 // test route
 app.get("/", (req, res) => {
-    res.send("CreatorBD Server is running 🚀");
+  res.send("CreatorBD Server is running 🚀");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
